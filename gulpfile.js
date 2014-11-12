cec487b04a468d06fd7056bb6ff055dfe37bd8cd
jolangreen <jolangreen@hotmail.com>
2014-11-11 20:35:46 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,30 @@
-// Project configuration
-//var project   = 'example_theme';
-
-// Initialization sequence
-
-//    
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
     compass = require('gulp-compass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
     livereload = require('gulp-livereload');
 
 
-// Compile Sass using compass
-gulp.task('styles', function() {
-   gulp.src('sass/*.scss')
-     .pipe(compass({ config_file: './config.rb', css: 'assets/css', sass: 'sass' }))
-     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-     .pipe(gulp.dest('assets/css'))
-     .pipe(livereload())
-     .pipe(rename({suffix: '.min'}))
-     .pipe(minifycss({ keepSpecialComments: 1 }))
-     .pipe(gulp.dest('assets/css'))
-     .pipe(notify({ message: 'Styles task complete' }));
- });
+//Images
+gulp.task('images', function() {
+    return gulp.src('images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('assets/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
 
 //Scripts
 gulp.task('scripts', function() {
-  gulp.src(['bower_components/angular/angular.js', 'bower_components/angular-route/angular-route.js', 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js', 'bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'js/*.js'])
+    return gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'js/*.js'])
     //.pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
+    //.pipe(jshint.reporter('default'))
     .pipe(concat('scripts.js'))
     .pipe(gulp.dest('assets/js'))
     .pipe(rename({suffix: '.min'}))
@@ -46,41 +33,44 @@ gulp.task('scripts', function() {
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-//Images
-gulp.task('images', function() {
-  gulp.src('images/**/*')
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('assets/images'))
-    .pipe(notify({ message: 'Images task complete' }));
-});
+
+// Compile Sass using compass
+gulp.task('styles', function() {
+   return gulp.src('sass/*.scss')
+     .pipe(compass({ config_file: './config.rb', css: '', sass: 'sass' }))
+     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+     .pipe(livereload())
+     .pipe(rename({suffix: '.min'}))
+     .pipe(minifycss({ keepSpecialComments: 1 }))
+     .pipe(gulp.dest('assets/css'))
+     .pipe(notify({ message: 'Styles task complete' }));
+ });
 
 
 //Move 'bower_components' styles to 'assets'
 gulp.task('bowerstyles', function() {
-  gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.css', './bower_components/fontawesome/css/font-awesome.css'])
-  .pipe(gulp.dest('./assets/css'));
+    return gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.css', './bower_components/fontawesome/css/font-awesome.css'])
+    .pipe(gulp.dest('./assets/css'));
 });
 
 //Move 'bower_components' fonts to 'assets'
 gulp.task('bowerfonts', function() {
-  gulp.src(['./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eot,svg,otf}', './bower_components/fontawesome/fonts/**/*.{ttf,woff,eot,svg,otf}', './fonts/**/*.{ttf,woff,eot,svg,otf}'])
-  .pipe(gulp.dest('./assets/fonts'));
+    return gulp.src(['./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eot,svg,otf}', './bower_components/fontawesome/fonts/**/*.{ttf,woff,eot,svg,otf}', './fonts/**/*.{ttf,woff,eot,svg,otf}'])
+    .pipe(gulp.dest('./assets/fonts'));
 });
-
 
 
 // Clean. Delete and replace all files in the destination folder.
 gulp.task('clean', function() {
-  gulp.src(['assets/css', 'assets/js', 'assets/images'], {read: false})
+  return gulp.src(['assets/css', 'assets/js', 'assets/images', 'assets/fonts'], {read: false})
     .pipe(clean());
 });
 
 
 
-
 // Default task
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'images', 'bowerstyles', 'bowerfonts');
+    gulp.start('images', 'scripts',  'styles', 'bowerstyles', 'bowerfonts');
 });
 
 
